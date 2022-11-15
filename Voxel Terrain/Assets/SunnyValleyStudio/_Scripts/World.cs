ea784e42dc84ccf3313f335e3e558e416359ae8d
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2022-11-15 16:59:13 +0000

    22.11.15 - 1659 - Completed SunnyValleyStudio: MINECRAFT in Unity - S2 - P15
    Chunks now load & unload as the player moves.
    
    - Completed SunnyValleyStudio tutorial: MINECRAFT in Unity - S2- P15

--- Voxel Terrain/Assets/SunnyValleyStudio/_Scripts/World.cs
+++ Voxel Terrain/Assets/SunnyValleyStudio/_Scripts/World.cs
@@ -11,55 +11,101 @@ namespace SunnyValleyStudio
         public int mapSizeChunks = 6;
         public int chunkSize = 16;
         public int chunkHeight = 100;
-        //public int waterThreshold = 50; // (moved to BiomeGenerator)
-        //public float noiseScale = 0.03f; // (moved to BiomeGenerator)
+        public int chunkDrawingRange = 8;
+
         public GameObject chunkPrefab;
 
         public TerrainGenerator terrainGenerator;
         public Vector2Int mapSeedOffset;
 
-        Dictionary<Vector3Int, ChunkData> chunkDataDictionary = new Dictionary<Vector3Int, ChunkData>();
-        Dictionary<Vector3Int, ChunkRenderer> chunkDictionary = new Dictionary<Vector3Int, ChunkRenderer>();
+        //Dictionary<Vector3Int, ChunkData> chunkDataDictionary = new Dictionary<Vector3Int, ChunkData>();
+        //Dictionary<Vector3Int, ChunkRenderer> chunkDictionary = new Dictionary<Vector3Int, ChunkRenderer>();
 
         public UnityEvent OnWorldCreated, OnNewChunksGenerated;
 
+        public WorldData worldData { get; private set; }
+
+        private void Awake()
+        {
+            worldData = new WorldData
+            {
+                chunkHeight = this.chunkHeight,
+                chunkSize = this.chunkSize,
+                chunkDataDictionary = new Dictionary<Vector3Int, ChunkData>(),
+                chunkDictionary = new Dictionary<Vector3Int, ChunkRenderer>()
+            };
+        }
+
         public void GenerateWorld()
         {
-            // Destroy previously generated world...
-            chunkDataDictionary.Clear();
-            foreach (ChunkRenderer chunk in chunkDictionary.Values)
-                Destroy(chunk.gameObject);
-            chunkDictionary.Clear();
+            GenerateWorld(Vector3Int.zero);
+        }
+
+        private void GenerateWorld(Vector3Int position)
+        {
+            WorldGenerationData worldGenerationData = GetPositionsThatPlayerSees(position);
+
+            // Remove the old chunks...
+            foreach (Vector3Int pos in worldGenerationData.chunkPositionsToRemove)
+                WorldDataHelper.RemoveChunk(this, pos);
+
+            // Remove the old chunks data...
+            foreach (Vector3Int pos in worldGenerationData.chunkDataToRemove)
+                WorldDataHelper.RemoveChunkData(this, pos);
 
             // Generate the chunks and voxel data...
-            for (int x = 0; x < mapSizeChunks; x++)
+            foreach (Vector3Int pos in worldGenerationData.chunkDataPositionsToCreate)
             {
-                for (int z = 0; z < mapSizeChunks; z++)
-                {
-                    ChunkData data = new ChunkData(chunkSize, chunkHeight, this, new Vector3Int(x * chunkSize, 0, z * chunkSize));
-                    //GenerateVoxels(data); // (v1)
-                    ChunkData newData = terrainGenerator.GenerateChunkData(data, mapSeedOffset); // (v2)
-                    chunkDataDictionary.Add(newData.worldPosition, data);
-                }
+                ChunkData data = new ChunkData(chunkSize, chunkHeight, this, pos);
+                ChunkData newData = terrainGenerator.GenerateChunkData(data, mapSeedOffset);
+                worldData.chunkDataDictionary.Add(pos, newData);
             }
 
             // Render the chunks...
-            foreach (ChunkData data in chunkDataDictionary.Values)
+            foreach (Vector3Int pos in worldGenerationData.chunkPositionsToCreate)
             {
+                ChunkData data = worldData.chunkDataDictionary[pos];
                 MeshData meshData = Chunk.GetChunkMeshData(data);
-                GameObject chunkObject = Instantiate(chunkPrefab, data.worldPosition, Quaternion.identity);
+                GameObject chunkObject = Instantiate(chunkPrefab, pos, Quaternion.identity);
                 ChunkRenderer chunkRenderer = chunkObject.GetComponent<ChunkRenderer>();
-
-                chunkDictionary.Add(data.worldPosition, chunkRenderer);
+                worldData.chunkDictionary.Add(pos, chunkRenderer);
                 chunkRenderer.InitializeChunk(data);
-                chunkRenderer.UpdateChunk(meshData);
+                chunkRenderer.UpdateChunk(meshData); 
             }
+
             OnWorldCreated?.Invoke();
+        }
+
+        internal void RemoveChunk(ChunkRenderer chunk)
+        {
+            chunk.gameObject.SetActive(false);
+        }
+
+        private WorldGenerationData GetPositionsThatPlayerSees(Vector3Int playerPosition)
+        {
+            List<Vector3Int> allChunkPositionsNeeded = WorldDataHelper.GetChunkPositionsAroundPlayer(this, playerPosition);
+            List<Vector3Int> allChunkDataPositionsNeeded = WorldDataHelper.GetDataPositionsAroundPlayer(this, playerPosition);
+            
+            List<Vector3Int> chunkPositionsToCreate = WorldDataHelper.SelectPositionsToCreate(worldData, allChunkPositionsNeeded, playerPosition);
+            List<Vector3Int> chunkDataPositionsToCreate = WorldDataHelper.SelectDataPositionsToCreate(worldData, allChunkDataPositionsNeeded, playerPosition);
+
+            List<Vector3Int> chunkPositionsToRemove = WorldDataHelper.GetUnneededChunks(worldData, allChunkPositionsNeeded);
+            List<Vector3Int> chunkDataToRemove = WorldDataHelper.GetUnneededData(worldData, allChunkDataPositionsNeeded);
+
+            WorldGenerationData data = new WorldGenerationData
+            {
+                chunkPositionsToCreate = chunkPositionsToCreate,
+                chunkDataPositionsToCreate = chunkDataPositionsToCreate,
+                chunkPositionsToRemove = chunkPositionsToRemove,
+                chunkDataToRemove = chunkDataToRemove
+            };
+            return data;
         }
 
         internal void LoadAdditionalChunkRequest(GameObject player)
         {
             Debug.Log($"[{name}] Load more chunks");
+            GenerateWorld(Vector3Int.RoundToInt(player.transform.position));
             OnNewChunksGenerated?.Invoke();
         }
 
@@ -115,13 +161,29 @@ namespace SunnyValleyStudio
             Vector3Int pos = Chunk.ChunkPositionFromBlockCoords(this, x, y, z);
             ChunkData containerChunk = null;
 
-            chunkDataDictionary.TryGetValue(pos, out containerChunk);
+            worldData.chunkDataDictionary   .TryGetValue(pos, out containerChunk);
 
             if (containerChunk == null)
                 return VoxelType.Nothing;
 
             Vector3Int blockInChunkCoordinates = Chunk.GetVoxelInChunkCoordinates(containerChunk, new Vector3Int(x, y, z));
             return Chunk.GetVoxelFromChunkCoordinates(containerChunk, blockInChunkCoordinates);
+        }
+
+        public struct WorldGenerationData
+        {
+            public List<Vector3Int> chunkPositionsToCreate;
+            public List<Vector3Int> chunkDataPositionsToCreate;
+            public List<Vector3Int> chunkPositionsToRemove;
+            public List<Vector3Int> chunkDataToRemove;
+        }
+
+        public struct WorldData
+        {
+            public Dictionary<Vector3Int, ChunkData> chunkDataDictionary;
+            public Dictionary<Vector3Int, ChunkRenderer> chunkDictionary;
+            public int chunkSize;
+            public int chunkHeight;
         }
     }
 }
@@ -130,3 +192,8 @@ namespace SunnyValleyStudio
 // Source: https://www.youtube.com/watch?v=L5obsaFeJPQ&ab_channel=SunnyValleyStudio
 // Source: https://www.youtube.com/watch?v=TOLlDa2XTbQ&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=3&ab_channel=SunnyValleyStudio
 // Source: S2 - P12 https://www.youtube.com/watch?v=ev4Nm50Ujok&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=12&ab_channel=SunnyValleyStudio
+// Source: S2 - P13 https://www.youtube.com/watch?v=AHkh5WNq528&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=13&ab_channel=SunnyValleyStudio
+// Source: S2 - P14 https://www.youtube.com/watch?v=AvowpcZssxU&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=14&ab_channel=SunnyValleyStudio
+// Source: S2 - P15 https://www.youtube.com/watch?v=qOcJDH0FfsY&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=15&ab_channel=SunnyValleyStudio
+
+
--- Voxel Terrain/Assets/SunnyValleyStudio/_Scripts/WorldDataHelper.cs
+++ Voxel Terrain/Assets/SunnyValleyStudio/_Scripts/WorldDataHelper.cs
@@ -2,6 +2,7 @@ using SunnyValleyStudio;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SunnyValleyStudio
@@ -17,7 +18,127 @@ namespace SunnyValleyStudio
                 z = Mathf.FloorToInt(position.z / (float)world.chunkSize) * world.chunkSize
             };
         }
+
+        internal static List<Vector3Int> GetChunkPositionsAroundPlayer(World world, Vector3Int playerPosition)
+        {
+            int startX = playerPosition.x - (world.chunkDrawingRange) * world.chunkSize;
+            int startZ = playerPosition.z - (world.chunkDrawingRange) * world.chunkSize;
+            int endX = playerPosition.x + (world.chunkDrawingRange) * world.chunkSize;
+            int endZ = playerPosition.z + (world.chunkDrawingRange) * world.chunkSize;
+
+            List<Vector3Int> chunkPositionsToCreate = new List<Vector3Int>();
+            for (int x = startX; x <= endX; x += world.chunkSize)
+            {
+                for (int z = startZ; z <= endZ; z += world.chunkSize)
+                {
+                    Vector3Int chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, 0, z));
+                    chunkPositionsToCreate.Add(chunkPos);
+                    //if (x >= playerPosition.x - world.chunkSize &&
+                    //    x <= playerPosition.x + world.chunkSize &&
+                    //    z >= playerPosition.z - world.chunkSize &&
+                    //    z >= playerPosition.z + world.chunkSize)
+                    //{
+                    //    for (int y =  -world.chunkHeight; y >= playerPosition.y - world.chunkHeight * 2; y -= world.chunkHeight)
+                    //    {
+                    //        chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, y, z));
+                    //        chunkPositionsToCreate.Add(chunkPos);
+                    //    }
+                    //}
+                }
+            }
+
+            return chunkPositionsToCreate;
+        }
+
+        internal static void RemoveChunkData(World world, Vector3Int pos)
+        {
+            world.worldData.chunkDataDictionary.Remove(pos);
+        }
+
+        internal static void RemoveChunk(World world, Vector3Int pos)
+        {
+            ChunkRenderer chunk = null;
+            if (world.worldData.chunkDictionary.TryGetValue(pos, out chunk))
+            {
+                world.RemoveChunk(chunk);
+                world.worldData.chunkDictionary.Remove(pos);
+            }
+        }
+
+        internal static List<Vector3Int> GetDataPositionsAroundPlayer(World world, Vector3Int playerPosition)
+        {
+            int startX = playerPosition.x - (world.chunkDrawingRange + 1) * world.chunkSize;
+            int startZ = playerPosition.z - (world.chunkDrawingRange + 1) * world.chunkSize;
+            int endX = playerPosition.x + (world.chunkDrawingRange + 1) * world.chunkSize;
+            int endZ = playerPosition.z + (world.chunkDrawingRange + 1) * world.chunkSize;
+
+            List<Vector3Int> chunkDataPosiionsToCreate = new List<Vector3Int>();
+            for (int x = startX; x <= endX; x += world.chunkSize)
+            {
+                for (int z = startZ; z <= endZ; z += world.chunkSize)
+                {
+                    Vector3Int chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, 0, z));
+                    chunkDataPosiionsToCreate.Add(chunkPos);
+                    //if (x >= playerPosition.x - world.chunkSize &&
+                    //    x <= playerPosition.x + world.chunkSize &&
+                    //    z >= playerPosition.z - world.chunkSize &&
+                    //    z >= playerPosition.z + world.chunkSize)
+                    //{
+                    //    for (int y = -world.chunkHeight; y >= playerPosition.y - world.chunkHeight * 2; y -= world.chunkHeight)
+                    //    {
+                    //        chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, y, z));
+                    //        chunkDataPosiionsToCreate.Add(chunkPos);
+                    //    }
+                    //}
+                }
+            }
+
+            return chunkDataPosiionsToCreate;
+        }
+
+        internal static List<Vector3Int> GetUnneededData(World.WorldData worldData, List<Vector3Int> allChunkDataPositionsNeeded)
+        {
+            return worldData.chunkDataDictionary.Keys
+                .Where(pos => allChunkDataPositionsNeeded.Contains(pos) == false && worldData.chunkDataDictionary[pos].modifiedByThePlayer == false)
+                .ToList();
+        }
+
+        internal static List<Vector3Int> GetUnneededChunks(World.WorldData worldData, List<Vector3Int> allChunkPositionsNeeded)
+        {
+            List<Vector3Int> positionToRemove = new List<Vector3Int>();
+
+            // Discard all worldData chunks not in allChunkPositionsNeeded...
+            foreach (var pos in worldData.chunkDictionary.Keys
+                .Where(pos => allChunkPositionsNeeded.Contains(pos) == false))
+            {
+                if (worldData.chunkDictionary.ContainsKey(pos)) // Note: 'this might be redundant'
+                {
+                    positionToRemove.Add(pos);
+                }
+            }
+
+            return positionToRemove;
+        }
+
+        internal static List<Vector3Int> SelectPositionsToCreate(World.WorldData worldData, List<Vector3Int> allChunkPositionsNeeded, Vector3Int playerPosition)
+        {
+            return allChunkPositionsNeeded
+                .Where(pos => worldData.chunkDictionary.ContainsKey(pos) == false)
+                .OrderBy(pos => Vector3.Distance(playerPosition, pos))
+                .ToList();
+        }
+
+        internal static List<Vector3Int> SelectDataPositionsToCreate(World.WorldData worldData, List<Vector3Int> allChunkDataPositionsNeeded, Vector3Int playerPosition)
+        {
+            return allChunkDataPositionsNeeded
+                .Where(pos => worldData.chunkDataDictionary.ContainsKey(pos) == false)
+                .OrderBy(pos => Vector3.Distance(playerPosition, pos))
+                .ToList();
+        }
     }
 }
 
 // Source: S2 - P12 https://www.youtube.com/watch?v=ev4Nm50Ujok&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=12&ab_channel=SunnyValleyStudio
+// Source: S2 - P13 https://www.youtube.com/watch?v=AHkh5WNq528&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=13&ab_channel=SunnyValleyStudio
+// Source: S2 - P14 https://www.youtube.com/watch?v=AvowpcZssxU&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=14&ab_channel=SunnyValleyStudio
+// Source: S2 - P15 https://www.youtube.com/watch?v=qOcJDH0FfsY&list=PLcRSafycjWFesScBq3JgHMNd9Tidvk9hE&index=15&ab_channel=SunnyValleyStudio
